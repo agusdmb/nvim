004f491c9d178ee5e68cde3fd7e8721ae4f61ef7
Agustin Marquez <agusdmb@gmail.com>
2022-04-24 13:11:47 +0200

    autopair pluggin

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -73,6 +73,8 @@ return packer.startup(function(use)
 
   use "mfussenegger/nvim-lint"
 
+  use "windwp/nvim-autopairs"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
