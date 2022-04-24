5c444d54d3fc6f5a10a1adfde64d8aee0c8ab6a4
Agustin Marquez <agusdmb@gmail.com>
2022-04-24 20:04:02 +0200

    improve telescope

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -65,6 +65,7 @@ return packer.startup(function(use)
 
   -- Telescope
   use "nvim-telescope/telescope.nvim"
+  use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make' }
 
   -- Treesitter
   use {
