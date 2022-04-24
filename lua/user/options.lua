e211aa86eecbfe5b4bab71121051c1948ddc9c63
Agustin Marquez <agusdmb@gmail.com>
2022-04-24 20:52:51 +0200

    nvim tree

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,6 +1,5 @@
 local options = {
   backup = false, -- creates a backup file
-  clipboard = "unnamedplus", -- allows neovim to access the system clipboard
   completeopt = { "menuone", "noselect" }, -- mostly just for cmp
   conceallevel = 0, -- so that `` is visible in markdown files
   fileencoding = "utf-8", -- the encoding written to a file
--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -85,6 +85,13 @@ return packer.startup(function(use)
 
   use "rcarriga/nvim-notify"
 
+  use {
+    'kyazdani42/nvim-tree.lua',
+    requires = {
+      'kyazdani42/nvim-web-devicons', -- optional, for file icon
+    }
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
