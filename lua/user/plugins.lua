cef7a5b340b9e6e32904fee69d62671801244d91
Agustin Marquez <agusdmb@gmail.com>
2022-04-24 22:46:00 +0200

    remove alpha

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -92,11 +92,6 @@ return packer.startup(function(use)
     }
   }
 
-  use {
-    'goolord/alpha-nvim',
-    requires = { 'kyazdani42/nvim-web-devicons' },
-  }
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
