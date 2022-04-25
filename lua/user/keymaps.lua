2ed476eec2cb3e940ad624b344d2d0adbd1f0383
Agustin Marquez <agusdmb@gmail.com>
2022-04-25 20:07:19 +0200

    comment out mapping to allow swapping words

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -49,7 +49,7 @@ keymap("v", ">", ">gv", opts)
 -- Move text up and down
 keymap("v", "<A-j>", ":m .+1<CR>==", opts)
 keymap("v", "<A-k>", ":m .-2<CR>==", opts)
-keymap("v", "p", '"_dP', opts)
+-- keymap("v", "p", '"_dP', opts)
 
 -- Visual Block --
 -- Move text up and down
