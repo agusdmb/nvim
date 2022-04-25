2cc51406b770705359ef003375ea3d12335b5b7c
Agustin Marquez <agusdmb@gmail.com>
2022-04-25 23:56:00 +0200

    lualine

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -94,6 +94,11 @@ return packer.startup(function(use)
 
   use "davidgranstrom/nvim-markdown-preview"
 
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
