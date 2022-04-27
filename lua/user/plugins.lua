0a7e730bd58722a45d08e75048a906f3c108e7db
Agustin Marquez <agusdmb@gmail.com>
2022-04-27 23:02:01 +0200

    Git plugins

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -113,6 +113,18 @@ return packer.startup(function(use)
     }
   end
   }
+
+  use {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+
+  use { "tpope/vim-fugitive", requires = { "tpope/vim-rhubarb" } }
+
+  use "rhysd/git-messenger.vim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
