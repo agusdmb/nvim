b4bf39a5e009aee298b3e14daf3adaa8eebe6f48
Agustin Marquez <agusdmb@gmail.com>
2022-05-06 16:09:14 +0200

    Impatient

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -178,6 +178,8 @@ return packer.startup(function(use)
   --   end
   -- }
 
+  use "lewis6991/impatient.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
