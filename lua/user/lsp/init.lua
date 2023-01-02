20078ff776b81345c51540c1e2cedf119cb52a3f
Agustin Marquez <agusdmb@gmail.com>
2023-01-02 11:35:27 +0100

    WIP

--- lua/user/lsp/init.lua
+++ lua/user/lsp/init.lua
@@ -1,9 +1,11 @@
+require("user.lsp.mason")
+
 local status_ok, _ = pcall(require, "lspconfig")
 if not status_ok then
   vim.notify("LSPConfig not found!", "error")
   return
 end
 
-require("user.lsp.mason")
 require("user.lsp.handlers").setup()
+
 require("user.lsp.null-ls")
