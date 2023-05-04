98673499d1c3a64d45ef17635949b4c3662e15dc
Agustin Marquez <agusdmb@gmail.com>
2023-05-04 22:05:47 +0200

    telescop load extensions lazygit

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -19,6 +19,7 @@ telescope.setup({
 -- To get fzf loaded and working with telescope, you need to call
 -- load_extension, somewhere after setup function:
 telescope.load_extension("fzf")
+telescope.load_extension("lazygit")
 
 local opts = { noremap = true, silent = true }
 local builtin = require('telescope.builtin')
