21a054aab16a3f4e42eac48fb29070bced29fb39
Agustin Marquez <agusdmb@gmail.com>
2023-05-04 16:19:47 +0200

    add a todo fixme xxx highlight

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -242,6 +242,19 @@ return packer.startup({
       end,
     }
 
+    -- Lua
+    use {
+      "folke/todo-comments.nvim",
+      requires = "nvim-lua/plenary.nvim",
+      config = function()
+        require("todo-comments").setup {
+          -- your configuration comes here
+          -- or leave it empty to use the default settings
+          -- refer to the configuration section below
+        }
+      end
+    }
+
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if packer_bootstrap then
