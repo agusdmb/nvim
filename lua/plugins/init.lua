2783d7cc722b985f32964cd5afb5042525c7ff5e
Agustin Marquez <agusdmb@gmail.com>
2023-05-05 12:08:42 +0200

    which key and neorg

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -253,4 +253,27 @@ return {
 
   -- 'rcarriga/nvim-dap-ui',
 
+
+  "folke/which-key.nvim",
+
+  {
+    "nvim-neorg/neorg",
+    build = ":Neorg sync-parsers",
+    opts = {
+      load = {
+        ["core.defaults"] = {},  -- Loads default behaviour
+        ["core.concealer"] = {}, -- Adds pretty icons to your documents
+        ["core.dirman"] = {      -- Manages Neorg workspaces
+          config = {
+            workspaces = {
+              notes = "~/notes",
+            },
+            default_workspace = "notes",
+          },
+        },
+      },
+    },
+    dependencies = { { "nvim-lua/plenary.nvim" } },
+  },
+
 }
