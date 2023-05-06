58285e6ee7e9d1807def0c4ec77e356ae6d28bb2
Agustin Marquez <agusdmb@gmail.com>
2023-05-06 22:48:21 +0200

    move harpoon

---
+++ lua/plugins/harpoon.lua
@@ -0,0 +1,10 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.api.nvim_set_keymap
+
+return {
+  "ThePrimeagen/harpoon",
+  config = function()
+    keymap("n", "<leader>h", ":lua require('harpoon.ui').toggle_quick_menu()<cr>", opts)
+    keymap("n", "<leader>H", ":lua require('harpoon.mark').add_file()<cr>", opts)
+  end
+}
--- lua/user/harpoon.lua
+++
@@ -1,6 +0,0 @@
-local opts = { noremap = true, silent = true }
-
-local keymap = vim.api.nvim_set_keymap
-
-keymap("n", "<leader>h", ":lua require('harpoon.ui').toggle_quick_menu()<cr>", opts)
-keymap("n", "<leader>H", ":lua require('harpoon.mark').add_file()<cr>", opts)
