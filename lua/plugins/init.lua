db2cd0577167a3f026e32c5cf24dc09768d0f261
Agustin Marquez <agusdmb@gmail.com>
2023-05-06 22:31:32 +0200

    move lualine

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -79,7 +79,14 @@ return {
 
   {
     "nvim-lualine/lualine.nvim",
-    dependencies = { "nvim-tree/nvim-web-devicons", opt = true }
+    dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
+    config = function()
+      require('lualine').setup {
+        options = {
+          globalstatus = true,
+        }
+      }
+    end
   },
 
   {
