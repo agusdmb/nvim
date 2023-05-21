a47056339cafae0af5d520cdc47e55f04a2bc762
Agustin Marquez <agusdmb@gmail.com>
2023-05-21 15:46:25 +0200

    remove some lsp mappings

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -36,10 +36,10 @@ local function setup_lspconfig()
 
   -- Global mappings.
   -- See `:help vim.diagnostic.*` for documentation on any of the below functions
-  vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
+  -- vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
   vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
   vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
-  vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
+  -- vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
   -- Use LspAttach autocommand to only map the following keys
   -- after the language server attaches to the current buffer
