4f62cc766841ac7737df29042c9d75eb3ceefe23
Agustin Marquez <agusdmb@gmail.com>
2023-06-08 11:04:31 +0200

    lualine lazy

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -26,10 +26,9 @@ return {
 	},
 	{
 		"nvim-lualine/lualine.nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons", opt = true },
-		config = function()
-			require("lualine").setup({ options = { globalstatus = true } })
-		end,
+		dependencies = { "nvim-tree/nvim-web-devicons"},
+		opts = { globalstatus = true },
+		event = "BufRead",
 	},
 	{
 		"alexghergh/nvim-tmux-navigation",
@@ -97,9 +96,27 @@ return {
 
 	{
 		"folke/twilight.nvim",
-		config = function()
-			require("twilight").setup({})
-		end,
+		opts = {
+			dimming = {
+				alpha = 0.25, -- amount of dimming
+				-- we try to get the foreground from the highlight groups or fallback color
+				color = { "Normal", "ffffff" },
+				term_bg = "000000", -- if guibg=NONE, this will be used to calculate text color
+				inactive = false, -- when true, other windows will be fully dimmed (unless they contain the same buffer)
+			},
+			context = 10, -- amount of lines we will try to show around the current line
+			treesitter = true, -- use treesitter when available for the filetype
+			-- treesitter is used to automatically expand the visible text,
+			-- but you can further control the types of nodes that should always be fully expanded
+			expand = { -- for treesitter, we we always try to expand to the top-most ancestor with these types
+				"function",
+				"method",
+				"table",
+				"if_statement",
+				"method_definition",
+			},
+			exclude = {}, -- exclude these filetypes
+		},
 		cmd = { "Twilight", "TwilightEnable" },
 	},
 	{
