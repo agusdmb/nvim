4e6bbe5dfc345877fecd7c35bd15f894cdd792cd
Agustin Marquez <agusdmb@gmail.com>
2023-06-17 17:17:45 +0200

    WIP

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -87,8 +87,8 @@ local function setup_lspconfig()
 	-- Global mappings.
 	-- See `:help vim.diagnostic.*` for documentation on any of the below functions
 	-- vim.keymap.set("n", "<space>e", vim.diagnostic.open_float)
-	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev)
-	vim.keymap.set("n", "]d", vim.diagnostic.goto_next)
+	vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Go to previous diagnostic" })
+	vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Go to next diagnostic" })
 	-- vim.keymap.set("n", "<space>q", vim.diagnostic.setloclist)
 
 	-- Use LspAttach autocommand to only map the following keys
@@ -102,9 +102,9 @@ local function setup_lspconfig()
 			-- Buffer local mappings.
 			-- See `:help vim.lsp.*` for documentation on any of the below functions
 			local opts = { buffer = ev.buf }
-			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, opts)
-			vim.keymap.set("n", "gd", vim.lsp.buf.definition, opts)
-			vim.keymap.set("n", "K", vim.lsp.buf.hover, opts)
+			vim.keymap.set("n", "gD", vim.lsp.buf.declaration, { buffer = ev.buf, desc = "Go to declaration" })
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, { buffer = ev.buf, desc = "Go to definition" })
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, { buffer = ev.buf, desc = "Hover" })
 			-- vim.keymap.set("n", "gi", vim.lsp.buf.implementation, opts)
 			-- vim.keymap.set("n", "<C-k>", vim.lsp.buf.signature_help, opts)
 			-- vim.keymap.set("n", "<space>wa", vim.lsp.buf.add_workspace_folder, opts)
@@ -112,13 +112,18 @@ local function setup_lspconfig()
 			-- vim.keymap.set("n", "<space>wl", function()
 			-- 	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
 			-- end, opts)
-			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, opts)
-			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, opts)
-			vim.keymap.set({ "n", "v" }, "<space>Ca", vim.lsp.buf.code_action, opts)
+			vim.keymap.set("n", "<space>D", vim.lsp.buf.type_definition, { buffer = ev.buf, desc = "Type definition" })
+			vim.keymap.set("n", "<space>rn", vim.lsp.buf.rename, { buffer = ev.buf, desc = "Rename" })
+			vim.keymap.set(
+				{ "n", "v" },
+				"<space>Ca",
+				vim.lsp.buf.code_action,
+				{ buffer = ev.buf, desc = "Code action" }
+			)
 			-- vim.keymap.set("n", "gr", vim.lsp.buf.references, opts)
 			vim.keymap.set("n", "<space>f", function()
 				vim.lsp.buf.format({ async = true })
-			end, opts)
+			end, { buffer = ev.buf, desc = "Format" })
 		end,
 	})
 end
@@ -207,7 +212,7 @@ return {
 	},
 	config = function()
 		-- Order matters here
-    basic_setup()
+		basic_setup()
 		setup_mason()
 		setup_mason_lspconfig()
 		setup_lspconfig()
