ba24224964d477a7a25b722f0ab4eb9981c5690a
Agustin Marquez <agusdmb@gmail.com>
2023-06-17 16:29:37 +0200

    farther improver start time

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -1,43 +1,45 @@
-local signs = {
-	{ name = "DiagnosticSignError", text = "" },
-	{ name = "DiagnosticSignWarn", text = "" },
-	{ name = "DiagnosticSignHint", text = "" },
-	{ name = "DiagnosticSignInfo", text = "" },
-}
+local function basic_setup()
+	local signs = {
+		{ name = "DiagnosticSignError", text = "" },
+		{ name = "DiagnosticSignWarn", text = "" },
+		{ name = "DiagnosticSignHint", text = "" },
+		{ name = "DiagnosticSignInfo", text = "" },
+	}
 
-for _, sign in ipairs(signs) do
-	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
-end
+	for _, sign in ipairs(signs) do
+		vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+	end
 
-local config = {
-	-- disable virtual text
-	virtual_text = true,
-	-- show signs
-	signs = {
-		active = signs,
-	},
-	update_in_insert = false,
-	underline = true,
-	severity_sort = true,
-	float = {
-		focusable = false,
-		style = "minimal",
+	local config = {
+		-- disable virtual text
+		virtual_text = true,
+		-- show signs
+		signs = {
+			active = signs,
+		},
+		update_in_insert = false,
+		underline = true,
+		severity_sort = true,
+		float = {
+			focusable = false,
+			style = "minimal",
+			border = "rounded",
+			source = "always",
+			header = "",
+			prefix = "",
+		},
+	}
+
+	vim.diagnostic.config(config)
+
+	vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
 		border = "rounded",
-		source = "always",
-		header = "",
-		prefix = "",
-	},
-}
+	})
 
-vim.diagnostic.config(config)
-
-vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, {
-	border = "rounded",
-})
-
-vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
-	border = "rounded",
-})
+	vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(vim.lsp.handlers.signature_help, {
+		border = "rounded",
+	})
+end
 
 local function setup_mason()
 	require("mason").setup({
@@ -205,6 +207,7 @@ return {
 	},
 	config = function()
 		-- Order matters here
+    basic_setup()
 		setup_mason()
 		setup_mason_lspconfig()
 		setup_lspconfig()
@@ -212,5 +215,5 @@ return {
 		setup_null_ls()
 	end,
 	event = "BufRead",
-	cmd = {"LspRestart", "Mason"},
+	cmd = { "LspRestart", "Mason" },
 }
