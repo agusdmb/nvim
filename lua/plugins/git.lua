87abb87c24e40742b751cdb7aa0ea568949a3cfd
Agustin Marquez <agusdmb@gmail.com>
2023-06-21 17:30:05 +0200

    git plugins to new module

---
+++ lua/plugins/git.lua
@@ -0,0 +1,37 @@
+return {
+	{
+		"kdheepak/lazygit.nvim",
+		config = function()
+			vim.keymap.set("n", "<leader>l", ":LazyGit<cr>", { silent = true, noremap = true })
+			require("telescope").load_extension("lazygit")
+		end,
+		keys = { { "<leader>l", nil, desc = "LazyGit" } },
+		cmd = { "LazyGit", "LazyGitFilterCurrentFile" },
+	},
+	{
+		"lewis6991/gitsigns.nvim",
+		config = function()
+			require("gitsigns").setup()
+		end,
+		event = "VeryLazy",
+	},
+	{
+		"tpope/vim-fugitive",
+		dependencies = {
+			"tpope/vim-rhubarb",
+		},
+		cmd = { "G", "Git", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete", "GBrowse" },
+	},
+	{
+		"rhysd/git-messenger.vim",
+		cmd = "GitMessenger",
+		keys = { { "<leader>gm", nil, desc = "Git Messenger" } },
+	},
+	{
+		"f-person/git-blame.nvim",
+		event = "VeryLazy",
+		config = function()
+			-- vim.g.gitblame_delay = 1000
+		end,
+	},
+}
--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -9,15 +9,6 @@ return {
 		event = "InsertEnter",
 	},
 	{
-		"kdheepak/lazygit.nvim",
-		config = function()
-			vim.keymap.set("n", "<leader>l", ":LazyGit<cr>", { silent = true, noremap = true })
-			require("telescope").load_extension("lazygit")
-		end,
-		keys = { { "<leader>l", nil, desc = "LazyGit" } },
-		cmd = { "LazyGit", "LazyGitFilterCurrentFile" },
-	},
-	{
 		"iamcco/markdown-preview.nvim",
 		build = "cd app && yarn install",
 		event = "VeryLazy",
@@ -44,25 +35,6 @@ return {
 			})
 		end,
 		event = "VeryLazy",
-	},
-	{
-		"lewis6991/gitsigns.nvim",
-		config = function()
-			require("gitsigns").setup()
-		end,
-		event = "VeryLazy",
-	},
-	{
-		"tpope/vim-fugitive",
-		dependencies = {
-			"tpope/vim-rhubarb",
-		},
-		cmd = { "G", "Git", "Gdiffsplit", "Gread", "Gwrite", "Ggrep", "GMove", "GDelete", "GBrowse" },
-	},
-	{
-		"rhysd/git-messenger.vim",
-		cmd = "GitMessenger",
-		keys = { { "<leader>gm", nil, desc = "Git Messenger" } },
 	},
 	{
 		"machakann/vim-highlightedyank",
@@ -280,13 +252,5 @@ return {
 			vim.keymap.set("n", "s", ":HopChar2MW<cr>")
 		end,
 		keys = { "s" },
-	},
-
-	{
-		"f-person/git-blame.nvim",
-		event = "VeryLazy",
-		config = function()
-			-- vim.g.gitblame_delay = 1000
-		end,
 	},
 }
