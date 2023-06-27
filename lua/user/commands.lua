55952e1eadbe73107eae07ed310bffa9aa00663c
Agustin Marquez <agusdmb@gmail.com>
2023-06-27 10:48:48 +0200

    Diagnostic On Off

--- lua/user/commands.lua
+++ lua/user/commands.lua
@@ -1,1 +1,11 @@
+local function disable_diagnostic_ui()
+	vim.diagnostic.config({ virtual_text = false, underline = false })
+end
+
+local function enable_diagnostic_ui()
+	vim.diagnostic.config({ virtual_text = true, underline = true })
+end
+
 vim.api.nvim_create_user_command("SplitLine", ":s/\\s\\+/\r/g | :nohlsearch", {})
+vim.api.nvim_create_user_command("DiagnosticOff", disable_diagnostic_ui, {})
+vim.api.nvim_create_user_command("DiagnosticOn", enable_diagnostic_ui, {})
