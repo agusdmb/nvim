b7188e87e07a971a2e0e9e7cd92a9d4f0f24239e
Agustin Marquez <agustinmarquez@Agustins-MacBook-Pro.local>
2023-08-24 21:19:54 +0200

    remove default telescope layout because it broke with some release

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -27,12 +27,12 @@ return {
 					hidden = true,
 				},
 			},
-			defaults = {
-				layout_strategy = "vertical",
-				layout_config = {
-					preview_height = 0.7,
-				},
-			},
+			-- defaults = {
+			-- 	layout_strategy = "vertical",
+			-- 	layout_config = {
+			-- 		preview_height = 0.7,
+			-- 	},
+			-- },
 		})
 
 		local builtin = require("telescope.builtin")
