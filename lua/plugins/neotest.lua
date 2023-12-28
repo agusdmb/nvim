c6ca08c8d6d8ddf59a7008725dee83b5b1e94360
Agustin Marquez <agusdmb@gmail.com>
2023-12-28 09:22:16 +0100

    neotest

--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -103,7 +103,6 @@ return {
     config = function()
       -- vim.builtin.dap.active = true
       require("neotest").setup({
-        quickfix = { enabled = true, open = false },
         adapters = {
           require("neotest-python")({
             -- Extra arguments for nvim-dap configuration
@@ -111,7 +110,7 @@ return {
             dap = { justMyCode = false },
             -- Command line arguments for runner
             -- Can also be a function to return dynamic values
-            args = { "--log-level", "DEBUG" },
+            -- args = { "--log-level", "DEBUG" },
             -- Runner to use. Will use pytest if available by default.
             -- Can be a function to return dynamic value.
             runner = "pytest",
