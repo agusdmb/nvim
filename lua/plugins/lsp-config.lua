b3759d18032b0342e20fbb01de9cd362fcd7b3b0
Agustin Marquez <agusdmb@gmail.com>
2024-04-12 13:13:48 +0200

    remove isort

--- lua/plugins/lsp-config.lua
+++ lua/plugins/lsp-config.lua
@@ -151,7 +151,7 @@ local function setup_null_ls()
 			-- null_ls.builtins.diagnostics.pylint,
 			null_ls.builtins.code_actions.refactoring,
 			null_ls.builtins.formatting.black,
-			null_ls.builtins.formatting.isort,
+			-- null_ls.builtins.formatting.isort,
 			null_ls.builtins.formatting.stylua,
 			null_ls.builtins.formatting.prettier,
 			-- null_ls.builtins.formatting.ruff,
