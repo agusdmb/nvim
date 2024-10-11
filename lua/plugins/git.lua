74c9d66953804168d1a97d5c33f13096ab795d34
Agustin Marquez <agusdmb@gmail.com>
2024-10-11 15:27:42 +0200

    WIP

--- lua/plugins/git.lua
+++ lua/plugins/git.lua
@@ -25,13 +25,17 @@ return {
 	{
 		"rhysd/git-messenger.vim",
 		cmd = "GitMessenger",
+    -- XXX: This config is not working
+    -- config = function()
+    --   vim.g.git_messenger_extra_blame_args = "-C -w"
+    -- end,
 		keys = { { "<leader>gm", nil, desc = "Git Messenger" } },
 	},
 	{
 		"f-person/git-blame.nvim",
 		event = "VeryLazy",
 		opts = {
-			enabled = false,
+			enabled = true,
 		},
 		keys = { { "<leader>gt", ":GitBlameToggle<CR>", desc = "Git Virtual Blame" } },
 	},
